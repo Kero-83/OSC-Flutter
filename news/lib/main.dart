28c0cf439e02220a5148c441f001942f441c3d36
Kero-83 <kerosarwat83@gmail.com>
2023-11-24 04:52:16 +0200

    save commit whould be removed

--- news/lib/main.dart
+++ news/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news/cubit/data_reciving_cubit.dart';
 import 'package:news/models/article_model.dart';
 import 'package:news/services/news_service.dart';
+import 'package:news/views/home_screen.dart';
 import 'package:news/widgets/bottom_navigation_bar.dart';
 
 void main() {
@@ -37,15 +38,14 @@ class _NewsAppState extends State<NewsApp> {
         await NewsService(Dio()).getTopHeadlines(category: 'health');
     articleScienceList =
         await NewsService(Dio()).getTopHeadlines(category: 'science');
-    await Future.delayed(const Duration(seconds: 2),
-        () => context.read<DataRecivingCubit>().dataRecived());
+    context.read<DataRecivingCubit>().dataRecived();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNavigationBarExample(),
+      home: HomeScreen(),
     );
   }
 }
